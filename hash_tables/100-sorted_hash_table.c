ac2906dbb3778f54b2cb7e9e6fa21abe9eedc45e
casey paul <casey.paul@atlasschool.com>
2025-02-13 02:26:22 +0000

    first function completed

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -1,2 +1,29 @@
+#include "hash_tables.h"
 
-working on it
+/**
+* shash_table_create - Creates a sorted hash table.
+* @size: The size of the array.
+*
+* Return: A pointer to the new created hash table, or NULL on a failure.
+*/
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *new_table;
+
+	new_table = malloc(sizeof(shash_table_t));
+	if (!new_table)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = calloc(size, sizeof(shash_node_t *));
+	new_table->shead = NULL;
+	new_table->stail = NULL;
+
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	return (new_table);
+}
---
+++ hash_tables/hash_table.h
@@ -0,0 +1,49 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+ * struct shash_node_s - Node of a hash table
+ * @key: The key, a string
+ * @value: The value corresponding to a key
+ * @next: Pointer to the next node in the list (for chaining)
+ * @sprev: Pointer to the previous element in the sorted list
+ * @snext: Pointer to the next element in the sorted list
+ */
+typedef struct shash_node_s
+{
+	char *key;  /* The key, string */
+	char *value; /* The value corresponding to a key */
+	struct shash_node_s *next; /* Ptr to next node in the list */
+	struct shash_node_s *sprev; /* Ptr to prev elem in the sorted list */
+	struct shash_node_s *snext; /* Ptr to next elem in the sorted list */
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table
+ * @size: The size of the array
+ * @array: Array of pointers to the first node of the linked list
+ * @shead: Pointer to the first element of the sorted linked list
+ * @stail: Pointer to the last element of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size; /* The size of the array */
+	shash_node_t **array; /* Array of Ptrs to first node of linked list */
+	shash_node_t *shead;  /* Ptr to first elem of the sorted linked list */
+	shash_node_t *stail;  /* Ptr to last elem of the sorted linked list */
+} shash_table_t;
+
+
+/* Function prototypes */
+/**
+ * shash_table_create - Creates a new sorted hash table
+ * @size: The size of the array
+ * Return: A pointer to the newly created hash table, or NULL on failure
+ */
+shash_table_t *shash_table_create(unsigned long int size);
